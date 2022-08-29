576c3dfaabcc1829f41235aa31dfcb2b66e6b068
fatihcaliss <fthcls@gmail.com>
2022-08-29 16:09:06 +0300

    first commit

---
+++ fscohort/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import index,student_list,student_add,student_update,student_delete,student_detail
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('list/',student_list , name='list'),
+    path('add/',student_add , name='add'),
+    path('update/<int:id>',student_update , name='update'),
+    path('delete/<int:id>',student_delete , name='delete'),
+    path('student/<int:id>', student_detail, name="detail"),
+]
